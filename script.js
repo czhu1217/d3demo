f0af599f8e8c0d12b4004246496fa7c78be56475
czhu1217 <cindyzhu0830@gmail.com>
2021-03-02 10:59:15 -0500

    fixed title, added rainbow handle for cmu

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ var svg = d3.select("#my_dataviz")
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
 
+
 svg.append("text")
     .attr("x", (width / 2))
-    .attr("y", 0 - (margin.top / 2))
+    .attr("y", 0 - (margin.top))
     .attr("dy", "1em")
     .style("text-anchor", "middle")
     .text("Top 10 CS School scores");
@@ -65,14 +66,33 @@ d3.csv("./data.csv", function(data){
         .attr("y", function(d) { return y(d.school); })
         .attr("width", function(d) { return x(50); })
         .attr("height", y.bandwidth() )
-        .attr("fill", "#4973c5")
+        .attr("fill", function (d){
+            if(d.school=="Carnegie Mellon University") return "red";
+            else return "steelblue";
+        })
 
     svg.selectAll("rect")
         .transition()
         .duration(800)
         .attr("x", x(50))
         .attr("width", function(d) { return x(d.score); })
-        .delay(function(d,i){console.log(i) ; return(i*100)})
+        .delay(function(d,i){return(i*100)})
 
+    var color = d3.scaleLinear()
+        .range(["red", "yellow", "green", "blue", "purple", "red"])
+        .domain([0, 10, 20, 30, 40, 50])
+        .interpolate(d3.interpolateHsl);
+
+    d3.timer(function(elapsed) {
+        d3.selectAll("rect")
+            .style("fill", function(d,i) { if(d.school=="Carnegie Mellon University") return color((elapsed/100) % 50);
+            else return "steelblue";})
+    });
+
+    svg.selectAll("rect")
+        .on("click", handleClick)
 
 })
+
+
+
