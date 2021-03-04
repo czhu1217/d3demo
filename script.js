a9240ddb5bdc2b69ce504b7e99f6d626804ca4c1
czhu1217 <cindyzhu0830@gmail.com>
2021-03-03 23:53:21 -0500

    added line chart with reference line

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ d3.csv("./aot.csv", function (data) {
         .append('path')
         .attr('d', arcGenerator)
         .attr('fill', function (d) { console.log(d); return (color(d.data.name)) })
-        .attr("stroke", "black")
+        .attr("stroke", "white")
         .style("stroke-width", "2px")
         .style("opacity", 0.7)
 
@@ -214,10 +214,107 @@ d3.csv("./aot.csv", function (data) {
             return (midangle < Math.PI ? 'start' : 'end')
         })
 
+})
+
+// set the dimensions and margins of the graph
+var margin3 = {top: 30, right: 30, bottom: 30, left: 60},
+    width3 = 600 - margin3.left - margin3.right,
+    height3 = 400 - margin3.top - margin3.bottom;
+
+// append the svg3 object to the body of the page
+var svg3 = d3.select("#line-plot")
+    .append("svg")
+    .attr("width", width3 + margin3.left + margin3.right)
+    .attr("height", height3 + margin3.top + margin3.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin3.left + "," + margin3.top + ")");
+
+svg3
+    .append('path')
+    .attr('d', d3.line()([[0, 0], [width3, 0]]))
+    .style("stroke-dasharray", ("15, 6"))
+    .attr('stroke', 'black')
+    .attr("stroke-width", "4")
+    .attr('fill', 'none');
 
 
+//Read the data
+d3.csv("data3.csv", function(data) {
+
+    // List of groups (here I have one group per column)
+    var allGroup = ["valueA", "valueB", "valueC"]
+
+    // add the options to the button
+    d3.select("#selectButton")
+        .selectAll('myOptions')
+        .data(allGroup)
+        .enter()
+        .append('option')
+        .text(function (d) { return d; }) // text showed in the menu
+        .attr("value", function (d) { return d; }) // corresponding value returned by the button
+
+    // A color scale: one color for each group
+    var myColor = d3.scaleOrdinal()
+        .domain(allGroup)
+        .range(d3.schemeSet2);
+
+    // Add X axis --> it is a date format
+    var x = d3.scaleLinear()
+        .domain([0,10])
+        .range([ 0, width3 ]);
+    svg3.append("g")
+        .attr("transform", "translate(0," + height3 + ")")
+        .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+        .domain( [0,20])
+        .range([ height3, 0 ]);
+    svg3.append("g")
+        .call(d3.axisLeft(y));
+
+    // Initialize line with group a
+    var line = svg3
+        .append('g')
+        .append("path")
+        .datum(data)
+        .attr("d", d3.line()
+            .x(function(d) { return x(+d.time) })
+            .y(function(d) { return y(+d.valueA) })
+        )
+        .attr("stroke", function(d){ return myColor("valueA") })
+        .style("stroke-width", 4)
+        .style("fill", "none")
+
+    // A function that update the chart
+    function update(selectedGroup) {
+
+        // Create new data with the selection?
+        var dataFilter = data.map(function(d){return {time: d.time, value:d[selectedGroup]} })
+
+        // Give these new data to update line
+        line
+            .datum(dataFilter)
+            .transition()
+            .duration(1000)
+            .attr("d", d3.line()
+                .x(function(d) { return x(+d.time) })
+                .y(function(d) { return y(+d.value) })
+            )
+            .attr("stroke", function(d){ return myColor(selectedGroup) })
+    }
+
+    // When the button is changed, run the updateChart function
+    d3.select("#selectButton").on("change", function(d) {
+        // recover the option that has been chosen
+        var selectedOption = d3.select(this).property("value")
+        // run the updateChart function with this selected option
+        update(selectedOption)
+    })
 
 })
+
 
 
 
